48b660eeef093de0642e3de34500e9eba7a8bb44
Sandeep Tiwari <sandeep8861@gmail.com>
2018-12-13 23:44:01 +0530

    chnages to pom for jar file build plugin

--- src/main/java/com/bnpp/interview/tenniskata/main/RunTennisGame.java
+++ src/main/java/com/bnpp/interview/tenniskata/main/RunTennisGame.java
@@ -1,8 +1,19 @@
 package com.bnpp.interview.tenniskata.main;
 
+import java.util.Scanner;
+
 public class RunTennisGame {
+	
+	/*static{
+		 Scanner readData = new Scanner(System.in);   
+		 System.out.println("Enter Game Fixtures in this format :<Player1Name Player2Name Player1Score Player2Score>");
+		 String str = readData.nextLine();
+		 String [] str1 = str.split(",");
+		 RunTennisGame.main(str1);
+		}*/
 
 	public static void main(String[] args) {
+		
 		Player player1=new Player(args[0]);
 		Player player2=new Player(args[1]);
 		
